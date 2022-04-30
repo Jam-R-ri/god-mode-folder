2a88a31b979f6baa15a7b11845e279a6a412a18d
Jam R <78709654+Jam-R-ri@users.noreply.github.com>
2022-04-30 10:23:41 -0700

    Add files via upload

---
+++ God Folder.bat
@@ -0,0 +1,6 @@
+:: Name a folder (the following line)
+:: .{ED7BA470-8E54-465E-825C-99712043E01C}
+@echo off
+cd %temp%
+md .{ED7BA470-8E54-465E-825C-99712043E01C}
+start .{ED7BA470-8E54-465E-825C-99712043E01C}
